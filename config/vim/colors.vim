3aed68fade43a24de03dc5176cff559f09829a8d
Jesse Woods <JesseDavidWoods@outlook.com>
2026-06-22 21:37:42 -0400

    Initial commit

---
+++ config/vim/colors.vim
@@ -0,0 +1,14 @@
+set termguicolors
+
+let g:tokyonight_style = 'night' " available: night, storm
+let g:tokyonight_enable_italic = 1
+
+colorscheme tokyonight
+
+" Make Vim background transparent
+highlight Normal ctermbg=NONE guibg=NONE
+highlight NonText ctermbg=NONE guibg=NONE
+highlight SignColumn ctermbg=NONE guibg=NONE
+highlight EndOfBuffer ctermbg=NONE guibg=NONE
+
+
---
+++ home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+let
+	dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+	create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+	configs = {
+		mango = "mango";
+		vim = "vim";
+		rofi = "rofi";
+		waybar = "waybar";
+		foot = "foot";
+	};
+	in
+
+	{	
+		home.username = "jwoods";
+		home.homeDirectory = "/home/jwoods";
+		programs.git.enable = true;
+		home.stateVersion = "26.05";
+		programs.bash = {
+			enable = true;
+			shellAliases = {
+				nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
+			};
+			
+		};
+		programs.ssh = {
+    			enable = true;
+    			matchBlocks = {
+      				"github.com" = {
+        				hostname = "github.com";
+        				identityFile = "~/.ssh/id_ed25519";
+      				};
+    			};
+  		};
+
+		xdg.configFile = builtins.mapAttrs
+			(name: subpath: {
+				source = create_symlink "${dotfiles}/${subpath}";
+				recursive = true;
+			})
+			configs;
+
+
+		home.packages = with pkgs; [
+			swaybg
+			rofi
+			waybar
+			pavucontrol
+			networkmanagerapplet
+			rust-analyzer
+			vim
+			fzf
+			nodejs
+			fd
+			git
+			gcc
+			clang-tools
+		];
+	}
